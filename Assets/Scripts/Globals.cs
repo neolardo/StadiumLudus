3660d934e4fe59acc697689d66eab447bce81cb9
Filep Leonardo <filep.leonardo@gmail.com>
2022-06-17 12:18:18 +0200

    Start warrior ground slam effect.

--- Assets/Scripts/Globals.cs
+++ Assets/Scripts/Globals.cs
@@ -1,8 +1,12 @@
+using System.Collections.Generic;
+using UnityEngine;
 /// <summary>
 /// A helper static class containing global variables.
 /// </summary>
 public static class Globals
 {
+    #region Global Constants
+
     // tags
     public const string CharacterTag = "Character";
     public const string HitBoxTag = "HitBox";
@@ -26,5 +30,44 @@ public static class Globals
     public const string AudioMixerMusicVolume = "MusicVolume";
     public const float AudioMixerMaximumDecibel = 0;
     public const float AudioMixerMinimumDecibel = -80;
+
+    #endregion
+
+    #region Helper Methods
+
+    /// <summary>
+    /// Generates a list of random indexes inside a given range.
+    /// </summary>
+    /// <param name="minInclusive">The minimum inclusive index of the range.</param>
+    /// <param name="maxExclusive">The maximum exclusive index of the range.</param>
+    /// <param name="numberOfIndexes">The number of indexes to generate from the range.</param>
+    /// <param name="unique">True if the indexes should be unique, otherwise false.</param>
+    /// <returns>The list of random indexes inside the given range.</returns>
+    public static List<int> GenerateRandomIndexes(int minInclusive, int maxExclusive, int numberOfIndexes, bool unique = true)
+    {
+        var indexList = new List<int>();
+        for (int i = minInclusive; i < maxExclusive; i++)
+        {
+            indexList.Add(i);
+        }
+        var resultList = new List<int>();
+        int count = numberOfIndexes;
+        if (unique)
+        {
+            count = Mathf.Min(numberOfIndexes, indexList.Count);
+        }
+        for (int i = 0; i < count; i++)
+        {
+            int ind = Random.Range(0, indexList.Count);
+            resultList.Add(indexList[ind]);
+            if (unique)
+            {
+                indexList.RemoveAt(ind);
+            }
+        }
+        return resultList;
+    }
+
+    #endregion
 
 }
